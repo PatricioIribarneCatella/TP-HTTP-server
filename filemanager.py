61a175fe65525fdf0487e98561b70a5e0f87f00b
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-09-23 00:45:09 -0300

    data volumen added in file system. FileManager writes and reads from fs

--- filemanager.py
+++ filemanager.py
@@ -16,27 +16,47 @@ class FileManager(object):
         self.data = {}
 
     def _get_handler(self, header, body):
-        if header['path'] not in self.data:
+        
+        data = None
+
+        uid = header['path'].split("/")[3]
+
+        try:
+            with open("/data/" + uid, "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
             return {'msg': 'not found'}, '404 ERROR'
-        return self.data.get(header['path']), '200 OK'
+
+        return data, '200 OK'
 
     def _post_handler(self, header, body):
+       
+        uid = header['path'].split("/")[3]
+        
+        with open("/data/" + uid, "w") as f:
+            json.dump(body, f)
+        f.close()
+
         if header['path'] not in self.data:
             self.data[header['path']] = body
-            uid = header['path'].split("/")[3]
             return {'id': uid}, '200 OK'
+        
         return {'msg': 'data already exists'}, '406 ERROR'
 
     def _put_handler(self, header, body):
+        
         if header['path'] not in self.data:
             return {'msg': 'not found'}, '404 ERROR'
         self.data[header['path']] = body
+        
         return {}, '200 OK'
 
     def _del_handler(self, header, body):
+        
         if header['path'] not in self.data:
             return {'msg': 'not found'}, '404 ERROR'
         del self.data[header['path']]
+        
         return {}, '200 OK'
 
     def handle_request(self, header, body):
